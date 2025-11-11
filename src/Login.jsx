567eb95aa68ee324802ef3be8553beea7b177f0c
Daniel Giger <danielg1111@verizon.net>
2025-11-11 18:16:19 -0500

    add right fields for login form

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,50 @@
 import { useState } from "react";
 
 export default function Login() {
+    const [form, setForm] = useState({
+        email: "",
+        password: ""
+    })
+
+    async function onSubmit(e) {
+        e.preventDefault()
+
+        const form_json = JSON.stringify(form);
+        console.log(`pretending to submit ${form_json}... maybe actually submitting...`)
+    }
+
     return (
         <main>
             <h1>
                 hola
             </h1>
+
+            <form onSubmit={onSubmit}>
+                <label htmlFor="email">Email</label>
+                <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    value={form.email}
+                    onChange={(e) => setForm({ ...form, email: e.target.value })}
+                    required
+                />
+
+                <label htmlFor="password">Password</label>
+                <input
+                    id="password"
+                    name="password"
+                    type="password"
+                    value={form.password}
+                    onChange={(e) => setForm({...form, password: e.target.value })}
+                    required
+                />
+
+                <button type="submit">
+                    Submit
+                </button>
+
+            </form>
         </main>
     )
 }
