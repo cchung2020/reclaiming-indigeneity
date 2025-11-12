12065bd2e4a957a025286833f7e30de9a130eda2
Daniel Giger <danielg1111@verizon.net>
2025-11-12 18:47:07 -0500

    switch API call to using async/await

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,15 +3,32 @@ import { useState } from 'react';
 
 const weather_url = "https://api.open-meteo.com/v1/forecast?latitude=3.3761&longitude=-74.8015&hourly=temperature_2m&forecast_days=1&temperature_unit=fahrenheit";
 
+
+async function get_cur_temp() {
+  const a = await fetch(weather_url);
+  const weather_json = await(a.json());
+  return weather_json.hourly.temperature_2m[0];
+}
+
+const cur_temp_outer = await get_cur_temp();
+console.log(cur_temp_outer);
+
+
 export default function Footer() {
   const [cur_temp, setTemp] = useState('');
 
-  fetch(weather_url)
-    .then(x => x.json())
-    .then(data => {
-        setTemp(data.hourly.temperature_2m[0]);
-      }
-    )
+  // fetch(weather_url)
+  //   .then(x => x.json())
+  //   .then(data => {
+  //       setTemp(data.hourly.temperature_2m[0]);
+  //     }
+  //   )
+
+  if (cur_temp != cur_temp_outer) {
+    setTemp(cur_temp_outer);
+  }
+  // const weather_json = await(a.json());
+  // console.log(weather_json)
 
   return (
     <footer>
