dfb82771b3a409acd74fd3c18a93cd7ba68ed88a
Daniel Giger <danielg1111@verizon.net>
2025-11-12 18:56:54 -0500

    add API call error handling

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,9 +5,17 @@ const weather_url = "https://api.open-meteo.com/v1/forecast?latitude=3.3761&long
 
 
 async function get_cur_temp() {
-  const a = await fetch(weather_url);
-  const weather_json = await(a.json());
-  return weather_json.hourly.temperature_2m[0];
+  try {
+    const resp = await fetch(weather_url);
+    if (!resp.ok) {
+      throw new Error("response status " + resp.status)
+    }
+    const weather_json = await(resp.json());
+    return weather_json.hourly.temperature_2m[0];
+  } catch(error) {
+    console.log('API call failed')
+    console.log(error.message);
+  }
 }
 
 const cur_temp_outer = await get_cur_temp();
